6ccc305fb67ce27ea016ed43cd1a9a1bf9aaa7ed
The Honored One <abhinavsingh.me5@gmail.com>
2026-09-24 19:46:47 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_blossom.mk
+    $(LOCAL_DIR)/arrow_blossom.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_blossom-user \
-    lineage_blossom-userdebug \
-    lineage_blossom-eng
+    arrow_blossom-user \
+    arrow_blossom-userdebug \
+    arrow_blossom-eng
